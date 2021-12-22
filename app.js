36a27ad411fe2e416509a4612a7c73463ca12b1c
Peter Busatti <peterbusatti@gmail.com>
2021-12-22 14:12:53 -0500

    start to add functionality to buttons and js equations

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+const numberButton = Array.from(document.getElementsByClassName("number"));
+const operatorButton = Array.from(document.getElementsByClassName("operator"));
+
+let lastNumber;
+let newNumber;
+let operator;
+
+let storedNumber;
+
+
+numberButton.forEach(item => item.addEventListener("click", getNumbers));
+
+
+function getNumbers() {
+        if (storedNumber === undefined) {
+            storedNumber = this.value;
+            console.log(storedNumber)
+        }
+        else if (!(storedNumber === undefined)) {
+            storedNumber += this.value;
+            console.log(storedNumber)
+        }
+}
+
+operatorButton.forEach(item => item.addEventListener("click", getOperator));
+
+function getOperator() {
+    if (operator === undefined && lastNumber === undefined) {
+        lastNumber = parseInt(storedNumber);
+        storedNumber = undefined;
+        operator = this.value;
+    }
+    else if (!(operator === undefined) && !(isNaN(lastNumber)) && newNumber === undefined) {
+        newNumber = parseInt(storedNumber);
+        let total = operate(lastNumber, newNumber, operator);
+        lastNumber = total;
+
+        operator = undefined;
+        storedNumber = undefined;
+        newNumber = undefined;
+        console.log(total);
+    }
+    else if (operator === undefined && !(isNaN(lastNumber)) && newNumber === undefined) {
+        operator = this.value;
+        newNumber = parseInt(storedNumber);
+        let total = operate(lastNumber, newNumber, operator);
+        lastNumber = total;
+
+        operator = undefined;
+        storedNumber = undefined;
+        newNumber = undefined;
+        console.log(total);
+    }
+}
+
 function add(a, b) {
     return a + b;
 };
@@ -30,4 +85,3 @@ function operate(a, b, operator) {
 }    
 
 let usrop = "/"
-console.log(operate(5, 2, usrop));
